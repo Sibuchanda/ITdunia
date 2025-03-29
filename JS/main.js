fa948a0a8f16ecadcd11be6902c31dccec10536a
Sibuchanda <sibuchanda457@gmail.com>
2025-03-29 23:07:31 +0530

    Modify the design

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,38 @@
+
+// ----------------- Auto Typing --------------
 let typed = new Typed('.auto-type',{
     strings: ["Serviced based", "Product based", "Startups"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
 })
+
+
+
+// ------------------- Accordion Logic -------------
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+// ------------------Pop up design -----------
+// Function to Open Popup
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "flex";
+}
+
+// Function to Close Popup
+function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+}
